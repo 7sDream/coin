b63612e278bb087795c6596fa9fe45e30e3b29db
7sDream <i@7sdre.am>
2019-03-25 19:55:00 +0800

    init

---
+++ Readme.md
@@ -0,0 +1,38 @@
+# Coin
+
+A script for toss a coin in the shell.
+
+## Origin
+
+Origin zsh script by [@Coder89757](https://www.v2ex.com/member/Coder89757)：https://www.v2ex.com/t/548388
+Fish port by [@coldnight](https://github.com/coldnight)：https://gist.github.com/coldnight/5c9964769e9a39bf17bb767c88e8c366
+
+I make this repo just for quick install with [fisher](https://github.com/jorgebucaran/fisher) or [omf](https://github.com/oh-my-fish/oh-my-fish).
+
+## Install
+
+`fisher add 7sDream/coin`
+
+or
+
+`omf install https://github.com/7sDream/coin` (Not tested, I don't use omf)
+
+## Configure
+
+Default coin emoji is 🌝 and 🌚, set `FISH_COIN_EMOJI_USE_FACE` to `0` to use 🌕 and 🌑.
+
+Default FPS is 24，you can change it with `FISH_COIN_FPS`. **Notice: show character need time too, so the higher the FPS, the bigger deviation, so lower than 30 is recommended**
+
+Default waiting time is 1s before show the result, change with `FISH_COIN_WAIT`(millisecond)。
+
+Example: 
+
+```fish
+set -g FISH_COIN_EMOJI_USE_FACE 0
+set -g FISH_COIN_FPS 30
+set -g FISH_COIN_WAIT_TIME 500
+```
+
+## License
+
+**I don't own this code, ask [@Coder89757](https://www.v2ex.com/member/Coder89757) for a license if you need.**
---
+++ coin.fish
@@ -0,0 +1,53 @@
+#!/usr/bin/fish
+# 抛硬币脚本
+# Author: Lucien-X
+# Origin: https://www.v2ex.com/t/548388
+# LastModified: 2019-02-22
+# LastModified: 2019-03-20 by coldnight: porting to fish shell
+# LastModified: 2019-03-25 by 7sDream: 
+#   - install with fisher or omf
+#   - fps, emoji and wait time configure
+# Usage: `fisher add 7sDream/coin` or `omf add 7sDream/coin`
+
+function coin
+
+  set -l MOON_EMOJI_LIGHT "🌝"
+  set -l MOON_EMOJI_DARK  "🌚"
+
+  if test "$FISH_COIN_EMOJI_USE_FACE" = "0"
+    set MOON_EMOJI_LIGHT "🌕"
+    set MOON_EMOJI_DARK  "🌑"
+  end
+
+  if test -z "$FISH_COIN_FPS"; or test "$FISH_COIN_FPS" -le 0
+    set FISH_COIN_FPS 24
+  end
+
+  set -l delay (math "1 / $FISH_COIN_FPS")
+
+  if test -z "$FISH_COIN_WAIT_TIME"; or test "$FISH_COIN_WAIT_TIME" -le 0
+    set FISH_COIN_WAIT_TIME 1000
+  end
+
+  set -l wait (math "floor($FISH_COIN_FPS * $FISH_COIN_WAIT_TIME / 1000)")
+
+  # Define backspace char
+  set -l BACKSPACE '\b\b'
+  # Loop animation for about 1s
+  set -l i 0
+  while test "$i" -lt "$wait"
+    for COIN in "$MOON_EMOJI_LIGHT" '🌖' '🌗' '🌘' "$MOON_EMOJI_DARK" '🌒' '🌓' '🌔'
+      echo -n "$BACKSPACE$COIN"
+      sleep $delay;
+      set i (math "$i" + 1)
+    end
+  end
+  # Roll the dice eventually
+  set -l v (math (random) % 2)
+  echo -en $BACKSPACE
+  if test "$v" -eq 0
+    echo "$MOON_EMOJI_LIGHT"
+  else
+    echo "$MOON_EMOJI_DARK"
+  end
+end
